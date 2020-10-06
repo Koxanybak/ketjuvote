c37c2bb9fd55dbd2f74684bd65b0777fcdda782f
Koxanybak <leevi.heikkila11@gmail.com>
2020-10-06 11:46:06 +0300

    sync

--- go.mod
+++ go.mod
@@ -3,7 +3,24 @@ module github.com/koxanybak/ketjuvote
 go 1.15
 
 require (
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/btcsuite/btcd v0.21.0-beta // indirect
+	github.com/davidlazar/go-crypto v0.0.0-20200604182044-b73af7476f6c // indirect
+	github.com/google/gopacket v1.1.18 // indirect
+	github.com/google/uuid v1.1.2 // indirect
+	github.com/joho/godotenv v1.3.0
+	github.com/koron/go-ssdp v0.0.2 // indirect
 	github.com/libp2p/go-libp2p v0.11.0
+	github.com/libp2p/go-libp2p-autonat v0.4.0 // indirect
+	github.com/libp2p/go-libp2p-circuit v0.4.0 // indirect
+	github.com/libp2p/go-libp2p-core v0.7.0
+	github.com/libp2p/go-libp2p-mplex v0.3.0 // indirect
+	github.com/libp2p/go-libp2p-swarm v0.3.1 // indirect
+	github.com/libp2p/go-netroute v0.1.4 // indirect
+	github.com/libp2p/go-sockaddr v0.1.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	golang.org/x/crypto v0.0.0-20201002170205-7f63de1d35b0 // indirect
+	golang.org/x/net v0.0.0-20201002202402-0a1ea396d57c // indirect
+	golang.org/x/sys v0.0.0-20201005172224-997123666555 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
-	"crypto/sha256"
-	"encoding/json"
-	"log"
-	"github.com/libp2p/go-libp2p"
+	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/libp2p/go-libp2p"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
 )
 
+// VoteData represents a single vote
 type VoteData struct {
-	eventID		int			`json:"eventID"`
+	EventID		int			`json:"eventID"`
 }
 
 // Block represents a single block in the blockchain
@@ -24,12 +36,18 @@ type Block struct {
 var BlockChain []Block
 
 // Calculate sha256 hash for the block
-func (block *Block) calculateHash() [32]byte {
-	jsonEnc, err := json.Marshal(*block)
+func calculateHash(ID int, data *VoteData) [32]byte {
+	byteData, err := json.Marshal(*data)
 	if err != nil {
 		panic(err)
 	}
-	return sha256.Sum256(jsonEnc)
+	buf := new(bytes.Buffer)
+	err = binary.Write(buf, binary.LittleEndian, ID)
+	if err != nil {
+		panic(err)
+	}
+	byteData = append(byteData, buf.Bytes()...)
+	return sha256.Sum256(byteData)
 }
 
 // Checks if the block is valid
@@ -37,7 +55,7 @@ func (block *Block) isValid(prev *Block) bool {
 	if prev.Hash != block.PrevHash {
 		return false
 	}
-	if block.Hash != block.calculateHash() {
+	if block.Hash != calculateHash(block.ID, &block.Data) {
 		return false
 	}
 	return true
@@ -57,16 +75,57 @@ func isChainValid(chain []Block) bool {
 func generateGenesisBlock() Block {
 	ID := 0
 	data := VoteData{ 0 }
+	hsh := calculateHash(ID, &data)
 	return Block{
 		ID,
 		data,
-		
+		hsh,
+		hsh,
 	}
 }
 
-func main() {
-	node, err := libp2p.New(context.Background(), )
-	if err !=  nil {
+// Creates a p2p node
+func createNode(listenPort string) (host.Host, error) {
+	priv, _, err := crypto.GenerateRSAKeyPair(2048, *new(io.Reader))
+	if err != nil {
 		panic(err)
 	}
+	fmt.Println(priv)
+
+	opts := []libp2p.Option{
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/127.0.0.1/tcp/%s", listenPort)),
+		libp2p.Identity(priv),
+	}
+	node, err := libp2p.New(context.Background(), opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return node, nil
+}
+
+// Stream handler
+func handler(s network.Stream) {
+	log.Println("Haloo")
+}
+
+func main() {
+	genesisBlock := generateGenesisBlock()
+	BlockChain = append(BlockChain, genesisBlock)
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Unable to load .env file: %v", err)
+	}
+	listenPort := flag.String("p", os.Getenv("LISTEN_PORT"), "port to listen to")
+	//targetAddr := flag.String("t", "", "target node to connect to")
+
+	node, err := createNode(*listenPort)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Listening on multiaddr: %v\n", node.Addrs())
+	node.SetStreamHandler("/p2p/1.0.0", handler)
+
+	select {}
 }
